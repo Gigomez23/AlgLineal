b8971047580cd917b87d1197503553e423899a5a
Gogogomez23 <com.gomez.com@gmail.com>
2024-10-03 22:41:47 -0600

    v3.3.0: se hacen mejoras a procesos y UI de calculadora de ecuaciones lineales (matrices)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Programa Versión:  3.2.7
+Programa Versión:  3.3.0
 Descripcion: archivo main
 Clase: Algebra Lineal
 Integrantes: Gabriel Gómez, Gessler Herrera, Gabriel Lacayo, Oliver Espinoza
@@ -13,5 +13,3 @@ if __name__ == "__main__":
     root.title("Calculadora Algebra Lineal")
     root.configure(fg_color=['gray92', 'gray14'])
     root.mainloop()
-
-    #just a test
