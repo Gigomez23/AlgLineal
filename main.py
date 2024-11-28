fe3edc7f6adba98a64ee6b5bc41fc64aec0caaed
Gogogomez23 <com.gomez.com@gmail.com>
2024-11-27 23:43:55 -0600

    v4.2.0: se agrga title menu y varios cambios de estilo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Programa Versión:  4.1.0
+Programa Versión:  4.2.0
 Descripcion: archivo main
 Clase: Algebra Lineal
 Integrantes: Gabriel Gómez, Gessler Herrera, Gabriel Lacayo, Oliver Espinoza
